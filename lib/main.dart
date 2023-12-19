f14b7c23aea60ccebd82cec07b97a6144d509e43
Ashisoma <okalionesmus@gmail.com>
2023-12-19 15:07:12 +0300

    feat: migration

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatefulWidget {
-  const Home({super.key});
 
-  @override
-  State<Home> createState() => _HomeState();
+void main() {
+  runApp(const MyApp());
 }
 
-class _HomeState extends State<Home> {
+//  HOME: NEWS FEED/ UPDTES ON EVENTS, CREATE NEW EVEN AND LIST OF EVENTS TO HAPPEN IN TFF AS WELL AS STUFF THEY ARE DOING
+//  PEAPLE IN THE PAGE AND THEIR EMAIL ADDRESS AND YOU CAN CLICK ON ONE AND EMAIL THEM
+// PROFILE OF THE USERS AND STUDENT PROFILE SHOWS THEIR PERFROMANCE TRENDS
+// NOTIFICATIONS IN FLUTTER FOR NEW EVENTS
+// HOW TO GUIDES WITH COLLAPSIBLE LIST TILES 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'Fleischer Foundation',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: const SplashScreen(),
+    );
   }
 }
