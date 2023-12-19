94d89a5056cd682b19aecd08ee3f9486ad577b4c
Ashisoma <okalionesmus@gmail.com>
2023-12-19 19:33:11 +0300

    migrate: upload completed successfully

---
+++ lib/auth/register_screen.dart
@@ -0,0 +1,190 @@
+import 'package:fleischer_foundation/auth/login_screen.dart';
+import 'package:fleischer_foundation/screens/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  final _formKey2 = GlobalKey<FormState>();
+  final TextEditingController firstNameEditingController =
+      new TextEditingController();
+  final TextEditingController seconfNameEditingController =
+      new TextEditingController();
+  final TextEditingController emailEditingController =
+      new TextEditingController();
+  final TextEditingController passwordNameEditingController =
+      new TextEditingController();
+  final TextEditingController confirmPasswordNameEditingController =
+      new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final registerButton = Material(
+      elevation: 5,
+      color: Colors.white,
+      // shape: const StadiumBorder(
+      // side: BorderSide(color: Colors.black, width: 2),
+      // ),
+      borderRadius: BorderRadius.circular(30),
+      child: MaterialButton(
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => SplashScreen()));
+        },
+        padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        minWidth: MediaQuery.of(context).size.width,
+        child: const Text("Register",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.black,
+            )),
+      ),
+    );
+
+    final confpasswordNameField = TextFormField(
+      autofocus: false,
+      obscureText: true,
+      controller: confirmPasswordNameEditingController,
+      // keyboardType: TextInputType.,
+      onSaved: (value) {
+        confirmPasswordNameEditingController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+        prefixIcon: const Icon(Icons.key),
+        contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+        hintText: "Confirm Password",
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+    );
+
+    final emailField = TextFormField(
+      autofocus: false,
+      controller: emailEditingController,
+      keyboardType: TextInputType.emailAddress,
+      onSaved: (value) {
+        emailEditingController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+        prefixIcon: const Icon(Icons.mail),
+        contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+        hintText: "Email",
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+    );
+    final passwordNameField = TextFormField(
+      autofocus: false,
+      obscureText: true,
+      controller: passwordNameEditingController,
+      // keyboardType: TextInputType.,
+      onSaved: (value) {
+        passwordNameEditingController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+        prefixIcon: const Icon(Icons.key),
+        contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+        hintText: "Password",
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.red,
+          ),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Form(
+              key: _formKey2,
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 100,
+                    child: Image.asset(
+                      "assets/logo2.png",
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  const Text(
+                    "Welcome!",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10),
+                  const Text(
+                    "Create your account",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  emailField,
+                  SizedBox(height: 10),
+                  passwordNameField,
+                  SizedBox(height: 10),
+                  confpasswordNameField,
+                  SizedBox(
+                    height: 15,
+                  ),
+                  registerButton,
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text("Do you have an account? "),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const LoginScreen()));
+                        },
+                        child: const Text(
+                          "Log in here",
+                          style: TextStyle(
+                            // color: Colors.redAccent,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,93 @@
+import 'package:fleischer_foundation/auth/login_screen.dart';
+import 'package:fleischer_foundation/auth/register_screen.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final loginBtn = Material(
+      elevation: 5,
+      color: Colors.white,
+      shape: const StadiumBorder(
+        side: BorderSide(color: Colors.white, width: 2),
+      ),
+      // borderRadius: BorderRadius.circular(30),
+      child: MaterialButton(
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => const LoginScreen()));
+        },
+        padding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+        minWidth: MediaQuery.of(context).size.width,
+        child: const Text("Login",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.black,
+            )),
+      ),
+    );
+
+    final registerBtn = Material(
+      elevation: 5,
+      color: Colors.white,
+      // shape: const StadiumBorder(
+      // side: BorderSide(color: Colors.black, width: 2),
+      // ),
+      borderRadius: BorderRadius.circular(30),
+      child: MaterialButton(
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const RegisterScreen()));
+        },
+        padding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+        minWidth: MediaQuery.of(context).size.width,
+        child: const Text("Register",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.black,
+            )),
+      ),
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 200,
+                child: Image.asset(
+                  "assets/logo2.png",
+                  fit: BoxFit.contain,
+                ),
+              ),
+              const SizedBox(
+                height: 100,
+              ),
+              loginBtn,
+              const SizedBox(
+                height: 15,
+              ),
+              registerBtn
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
